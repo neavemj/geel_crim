e12a26205e9cda74033bf560b66c908ce181a9e4
Neave <nea040@csiro.au>
2016-07-26 19:11:32 +1000

    added function to extract postcode location from json

--- crime.py
+++ crime.py
@@ -16,6 +16,21 @@ OG_2014 = pdf[(pdf.Postcode == 3226) & (pdf.April_to_March == 2014)]
 
 c_map = folium.Map(location=[-38.1417, 144.4265], tiles="Cartodb Positron",
                    zoom_start=11)
-c_map.choropleth(geo_path="./data/postcode_boundaries/POA_2011_VIC.json")                   
+                   
+# use function to cut down json file to Geelong region postcodes
+                   
+def cut_json(fl, region):
+    with open(fl) as f:
+        for line in f:
+            if line.startswith('{ "type"'):
+                cols = line.strip().split('"')
+                postcode = cols[9]
+                print postcode
+                break
+
+cut_json("./data/postcode_boundaries/POA_2011_VIC.json", 4)
+
+
+#c_map.choropleth(geo_path="./data/postcode_boundaries/POA_2011_VIC.json")                   
                    
 c_map.save("crime_map.html")
